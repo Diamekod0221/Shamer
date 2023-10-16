ef22125f7847fa1633e0c7b042faab5acf6b96aa
Diamekod0221 <diamekod0221@gmail.com>
2023-10-16 15:30:02 +0200

    Reverted back to .properties dataSource

--- src/main/java/com/lolshame/LoLShame/AppConfig.java
+++ src/main/java/com/lolshame/LoLShame/AppConfig.java
@@ -2,25 +2,14 @@ package com.lolshame.LoLShame;
 
 import com.lolshame.LoLShame.player.results.ResultsCalculator;
 import com.lolshame.LoLShame.player.results.ResultsCalculatorAveraged;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 import java.time.Clock;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/shamer");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("R2sHT3PkMrq7nN1SbvRj");
-        return dataSourceBuilder.build();
-    }
 
 
     @Bean
